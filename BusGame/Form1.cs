6c1edf5aa48d122c5e45770557e2918a05c5ecb1
Josh Heidenreich <josh.sickmate@gmail.com>
2012-06-08 18:32:38 +0930

    Ships to have health

--- BusGame/Form1.cs
+++ BusGame/Form1.cs
@@ -16,8 +16,8 @@ namespace BusGame
         Bitmap cow;
         Bitmap bg;
 
-        Point player1;
-        Point player2;
+        Ship player1;
+        Ship player2;
         List<Bullet> bullets;
         List<Cow> cows;
         Random r;
@@ -42,8 +42,13 @@ namespace BusGame
         }
 
         public void newgame() {
-            player1 = new Point(100, (this.ClientSize.Height - ship.Height) / 2);
-            player2 = new Point(this.Width - ship.Width - 100, (this.ClientSize.Height - ship.Height) / 2);
+            player1 = new Ship();
+            player1.p = new Point(100, (this.ClientSize.Height - ship.Height) / 2);
+            player1.h = 5;
+
+            player2 = new Ship();
+            player2.p = new Point(this.Width - ship.Width - 100, (this.ClientSize.Height - ship.Height) / 2);
+            player2.h = 5;
 
             bullets = new List<Bullet>();
             cows = new List<Cow>();
@@ -54,20 +59,14 @@ namespace BusGame
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Rectangle p1 = new Rectangle(player1.X, player1.Y, ship.Width, ship.Height);
-            Rectangle p2 = new Rectangle(player2.X, player2.Y, ship.Width, ship.Height);
+            Rectangle p1 = new Rectangle(player1.p.X, player1.p.Y, ship.Width, ship.Height);
+            Rectangle p2 = new Rectangle(player2.p.X, player2.p.Y, ship.Width, ship.Height);
 
             foreach (Bullet b in bullets) {
                 b.p.X += b.v.X;
+            }
 
-                if (p1.Contains(b.p)) {
-                    win("Player 2");
-                    return;
-                } else if (p2.Contains(b.p)) {
-                    win("Player 1");
-                    return;
-                }
-
+            foreach (Bullet b in bullets) {
                 foreach (Cow c in cows) {
                     Rectangle p = new Rectangle(c.p.X, c.p.Y, cow.Width, cow.Height);
                     if (p.Contains(b.p)) {
@@ -79,6 +78,24 @@ namespace BusGame
 
             bullets.RemoveAll(b => b.h == 0);
             cows.RemoveAll(c => c.h == 0);
+
+            foreach (Bullet b in bullets) {
+                if (p1.Contains(b.p)) {
+                    player1.h--;
+                    b.h--;
+                    return;
+                } else if (p2.Contains(b.p)) {
+                    player2.h--;
+                    b.h--;
+                    return;
+                }
+            }
+
+            if (player1.h == 0 && player2.h == 0) win("No one");
+            if (player1.h == 0) win("Player 2");
+            if (player2.h == 0) win("Player 1");
+
+            bullets.RemoveAll(b => b.h == 0);
 
             if (r.Next(1, 50) == 1) {
                 Cow c = new Cow();
@@ -106,9 +123,9 @@ namespace BusGame
                 g.FillRectangle(b, new Rectangle(0, 0, this.Size.Width, this.Size.Height));
             }
 
-            g.DrawImage(ship, player1);
+            g.DrawImage(ship, player1.p);
             ship.RotateFlip(RotateFlipType.RotateNoneFlipX);
-            g.DrawImage(ship, player2);
+            g.DrawImage(ship, player2.p);
             ship.RotateFlip(RotateFlipType.RotateNoneFlipX);
 
             foreach (Cow c in cows) {
@@ -124,28 +141,33 @@ namespace BusGame
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode) {
-                case Keys.Q: player1.Y -= 10; break;
-                case Keys.A: player1.Y += 10; break;
+                case Keys.Q: player1.p.Y -= 10; break;
+                case Keys.A: player1.p.Y += 10; break;
                 case Keys.E: fire(player1, 30); break;
 
-                case Keys.O: player2.Y -= 10; break;
-                case Keys.L: player2.Y += 10; break;
+                case Keys.O: player2.p.Y -= 10; break;
+                case Keys.L: player2.p.Y += 10; break;
                 case Keys.U: fire(player2, -30); break;
 
                 case Keys.Escape: Application.Exit(); break;
             }
         }
 
-        private void fire(Point player, int velx)
+        private void fire(Ship player, int velx)
         {
             Bullet b = new Bullet();
-            b.p = player;
+            b.p = player.p;
             b.p.X += ship.Width * Math.Sign(velx);
             b.p.Y += ship.Height / 2;
             b.v = new Point(velx, 0);
             b.h = 1;
             bullets.Add(b);
         }
+    }
+
+    public class Ship {
+        public Point p;
+        public int h;
     }
 
     public class Bullet
